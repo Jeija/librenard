1c69e88c466cc17ed4caa7146c014ef84c1ee193
Jeija <norrepli@gmail.com>
2018-10-28 08:33:01 +0100

    Fix uplink decoding bug: Correct payload length calculation

--- src/uplink.c
+++ src/uplink.c
@@ -411,7 +411,7 @@ sfx_uld_err sfx_uplink_decode(sfx_ul_encoded to_decode, sfx_ul_plain *uplink_out
 	uint8_t flags = getvalue_nibbles(frame_plain, 3, 1);
 	uint8_t maclen = SFX_UL_MIN_MACLEN + (uplink_out->singlebit ? 0 : flags >> 2);
 	uplink_out->request_downlink = flags & 0b0010 ? true : false;
-	uplink_out->payloadlen = packetlen_bytes - (SFX_UL_FLAGLEN_NIBBLES + SFX_UL_SNLEN_NIBBLES + SFX_UL_DEVIDLEN_NIBBLES + SFX_UL_CRCLEN_NIBBLES) / 2 - maclen;
+	uplink_out->payloadlen = packetlen_bytes - (SFX_UL_FLAGLEN_NIBBLES + SFX_UL_SNLEN_NIBBLES + SFX_UL_DEVIDLEN_NIBBLES) / 2 - maclen;
 
 	// Copy frame's payload to uplink_out (decoded properties)
 	if (!uplink_out->singlebit)
